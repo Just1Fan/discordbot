76e41c350a664c20eed2559f8475b78116600428
root@lux:~# <44855365+rootluxIT@users.noreply.github.com>
2019-09-24 20:34:45 -0700

    Create Commands.js

---
+++ Commands.js
@@ -0,0 +1,75 @@
+feel free to edit to make them personal
+
+// Commands
+
+client.on('message', msg => {
+ if (msg.content === '!rank') {
+ msg.reply('you are RANK 1 ');
+ }
+ });
+
+
+client.on('message', msg => {
+ if (msg.content === 'GIDEON show me what I can do') {
+ msg.reply('These are your options for now !coms,!rank,!twitch');
+ }
+ });
+
+ client.on('message', msg => {
+ if (msg.content === 'GIDEON') {
+ msg.reply('Hello, I am GIDEON and right now I am in beta. These are my primary functions, I give commands such as !coms and I will be taking care of your leveling rank in the server. ');
+ }
+ });
+
+
+client.on('message', msg => {
+ if (msg.content === '!meep') {
+ msg.reply('MEEP MY BOIZ MMMMEEEEEEEEPPPP');
+ }
+ });
+
+
+client.on('message', msg => {
+ if (msg.content === '!waven') {
+ msg.reply('NO U!');
+ }
+ });
+
+
+client.on('message', msg => {
+ if (msg.content === '!prime') {
+ msg.reply('Did you know you can link your Amazon Prime account to your Twitch account? Its called Twitch Prime and you can sub to your favorite streamers for FREE for a month! So if you like my content and would like to support me then get Twitch Prime and sub! If you have Twitch Prime already then feel free to support me!');
+ }
+ });
+
+
+
+client.on('message', msg => {
+ if (msg.content === '!social') {
+ msg.reply('Github https://github.com/RetroSec ,Twitter https://twitter.com/Retr0SecIT ,Twitch https://www.twitch.tv/punchimari');
+ }
+ });
+
+
+
+
+client.on('message', msg => {
+ if (msg.content === '!copy') {
+ msg.reply('mei is a toxic kid who happens to play games');
+ }
+ });
+
+
+client.on('message', msg => {
+ if (msg.content === 'me when I code') {
+ msg.reply('(╯°□°）╯︵ ┻━┻ ');
+ }
+ });
+
+
+
+client.on('message', msg => {
+ if (msg.content === '!live') {
+ msg.reply('@everyone Im live over on https://www.twitch.tv/punchimari');
+ }
+ });
